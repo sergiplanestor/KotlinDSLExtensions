d1c495efd6527b0db722efb7f467969fbb03b646
Sergi Planes <59670429+sergiplanestor@users.noreply.github.com>
2022-01-24 00:16:43 +0100

    Create SdkConfig.kt

---
+++ buildSrc/src/main/kotlin/SdkConfig.kt
@@ -0,0 +1,8 @@
+object SdkConfig {
+    const val min = 24
+    const val compile = 31
+    const val target = compile
+    const val buildTools = "30.0.2"
+    const val appId = "com.revolhope.compose_utils"
+    const val instrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+}
